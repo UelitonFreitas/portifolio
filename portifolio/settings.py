0fbbf1b01ab21a265dd7f0883cd9cd51a56f234b
unknown <freitas.ueliton@gmail.com>
2015-09-13 11:09:28 -0300

    configurando

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portifolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portifolio',
+        'USER': 'freitasueliton',
+        'PASSWORD': '1nter36899484821',
+        'HOST': 'freitasueliton.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
